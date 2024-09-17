a55a4bccbaf8483962348f8d61ef370d8c7bb7a5
connorhelms <156145464+connorhelms@users.noreply.github.com>
2024-09-17 10:50:18 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// Define an array to hold the blog posts
+
 $blogPosts = [
     [
         'title' => 'Team Member 1',
@@ -19,7 +19,7 @@ $blogPosts = [
         'author' => 'Khalil Rountree',
         'date' => '2024-09-17'
     ],
-    // You can add more blog posts here...
+    // You can add more blog posts here
 ];
 ?>
 
@@ -43,7 +43,7 @@ $blogPosts = [
             <?php endforeach; ?>
         </div>
     </div>
-    <!-- Bootstrap JS (Optional) -->
+    
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
